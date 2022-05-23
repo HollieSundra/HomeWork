1ef2877723db964e4b6e4cd7b4023c2146063f91
HollieSundra <holliesundra@gmail.com>
2022-05-23 11:26:36 -0700

    Expanded homework

--- MyFirstApplication/src/MyClass.java
+++ MyFirstApplication/src/MyClass.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class MyClass {
     public static void main(String [] args) {
         System.out.println("Hello World!!");
+
+        System.out.println("What is your name?");
+
+        Scanner reader = new Scanner(System.in);
+        String name = reader.nextLine();
+        System.out.println("Hello, " + name + "!");
+        reader.close();
     }
 }
