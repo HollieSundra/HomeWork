54a8e8804b06543969e37e0e7d5f0a06953a9ada
HollieSundra <holliesundra@gmail.com>
2022-07-07 22:30:33 -0700

    BlackJackUpdate7722

--- BlackJack/src/Blackjack.java
+++ BlackJack/src/Blackjack.java
@@ -1,44 +1,176 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-/*
-            ::::: Card Overview :::::
-    Internally, cards are treated as an integer.
-    1: Ace
-    2-10: Actual cards, 2 thru 10
-    11: Jack
-    12: Queen
-    13: King
 
-    - Card numbers should not be directly treated as their score.
-    - A king should still be worth 10, for instance.
-    - Aces are always worth 11, unless causing a bust, then their worth is a 1.
-    - The dealer should stay when they have a 17 or higher.
- */
 public class Blackjack {
+    enum Card {
+        Ace(1), Two(2), Three(3), Four(4), Five(5),
+        Six(6), Seven(7), Eight(8), Nine(9), Ten(10),
+        Jack(10), Queen(10), King(10);
 
-    // Hands store card numbers for a given player.
-    private ArrayList<String> userHand = new ArrayList<>();
-    private ArrayList<String> dealerHand = new ArrayList<>();
+        private int value;
 
-    // Triggers the start of a game loop.
-    public void PlayGame() {
+        Card(int value){
+            this.value = value;
+        }
+
+        public static Card valueOf(int value) {
+            for (Card card : values()) {
+                if (card.value == value) return card;
+            }
+            System.out.println("No card exists for value: " + value);
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case King:
+                    return "King";
+                case Queen:
+                    return "Queen";
+                case Jack:
+                    return "Jack";
+                default:
+                    return "" + value;
+            }
+        }
+    }
+
+
+    private ArrayList<Card> userHand = new ArrayList<>();
+    private ArrayList<Card> dealerHand = new ArrayList<>();
+
+
+    public void PlayGame() throws InterruptedException {
         System.out.println("Welcome to BlackJack.");
 
-        // Get a random card and give it to the player.
+
         var randomCard = DrawRandomCard();
+        userHand.add(randomCard);
         userHand.add(randomCard);
 
         // Show the user their hand.
-        DisplayHand(userHand);
+        DisplayHand(userHand, "user");
 
         // Get a random card and give it to the dealer.
         dealerHand.add(DrawRandomCard());
+        dealerHand.add(DrawRandomCard());
 
         // Show the user the dealer's hand.
-        DisplayHand(dealerHand);
+        DisplayHand(dealerHand, "dealer");
 
 
-        /*
+
+        Scanner input = new Scanner(System.in);
+
+        boolean userStay = false;
+        boolean dealerStay = false;
+
+        while (!userStay) {
+            System.out.println("Hit or Stay?: ");
+            String choice = input.nextLine();
+
+            if (choice.equalsIgnoreCase("hit")) {
+                Card userNewCard = DrawRandomCard();
+                userHand.add(userNewCard);
+                System.out.println("You flipped a " + userNewCard);
+                DisplayHand(userHand, "user");
+                if (getHandTotal(userHand) > 21) {
+                    System.out.println("Bust!");
+                    break;
+                }
+            } else if (choice.equalsIgnoreCase("Stay")) {
+                userStay = true;
+                DisplayHand(userHand, "user");
+
+            } else {
+                System.out.println("Do not understand?");
+            }
+        }
+        while (!dealerStay) {
+
+            Thread.sleep(2000);
+
+            if (getHandTotal(dealerHand) <= getHandTotal(userHand)) {
+                Card dealerNewCard = DrawRandomCard();
+                dealerHand.add(dealerNewCard);
+                if (getHandTotal(dealerHand) > 21) {
+                    System.out.println("Dealer Busts, User wins!");
+                    DisplayHand(dealerHand, "dealer");
+                    break;
+                }
+
+            } else {
+                System.out.println("Dealer stays!");
+                DisplayHand(dealerHand, "dealer");
+                break;
+            }
+        }
+        while (!dealerStay && !userStay) {
+
+            if (getHandTotal(dealerHand) > getHandTotal(userHand)) {
+                System.out.println("Dealer won!");
+                break;
+            }
+        }
+
+        System.out.println("Game over.");
+    }
+
+    // Draws a random card from a deck with an unlimited supply.
+    private static Card DrawRandomCard() {
+        int value = (int) (Math.random() * 13) + 1;
+        switch(value) {
+            case 13:
+                return Card.King;
+            case 12:
+                return Card.Queen;
+            case 11:
+                return Card.Jack;
+
+            default:
+                return Card.valueOf(value);
+        }
+
+    }
+
+    // Called to show a given hand to the user, via the console.
+    private static void DisplayHand(ArrayList<Card> hand, String prefix) {
+        System.out.print(prefix + " : ");
+
+        for (int i = 0; i < hand.size(); i++)
+        {
+            System.out.println(hand.get(i) + " ");
+        }
+        System.out.println();
+
+        System.out.println("Total: " + getHandTotal(hand));
+    }
+
+    // Returns the highest possible score for a given hand.
+    private static int getHandTotal(ArrayList<Card> hand) {
+        // TODO: Calculate the total value of a hand. If there is an Ace, use the higher value unless it causes a bust.
+        // Example: [A 8] should return 19. [A 6 9] should return 16.
+        int handTotal = 0;
+
+        for (int i = 0; i < hand.size(); i++){
+            Card card = hand.get(i);
+            int cardValue = card.value;
+            handTotal = handTotal + cardValue;
+        }
+        return handTotal;
+    }
+
+    // Returns true if a value is considered a bust. Returns false if the value is still playable.
+    private static boolean isValueBust(int handTotal) {
+
+        return handTotal > 21;
+
+
+    }
+}
+/*
             TODO: Write the game loop.
             Setup:
                 User and dealer should both be given 2 cards to start.
@@ -55,60 +187,3 @@ public class Blackjack {
                 The winner is the player with the highest, non-bust score.
                 The winner should be displayed.
          */
-
-
-
-        System.out.println("Game over.");
-    }
-
-    // Draws a random card from a deck with an unlimited supply.
-    private static String DrawRandomCard() {
-        int value = (int) (Math.random() * 13) + 1;
-        switch (value) {
-            case 1:
-                return "Ace";
-            case 11:
-                return "Jack";
-            case 12:
-                return "Queen";
-            case 13:
-                return "King";
-        }
-        return String.valueOf(value);
-    }
-
-    // Called to show a given hand to the user, via the console.
-    private static void DisplayHand(ArrayList<String> hand) {
-        // TODO: Nicely output an entire hand to the console.
-        // Example: Cards: 3, 7, K -- Total: 20
-        for (int i = 0; i < hand.size(); i++)
-        {
-            System.out.println(hand.get(i) + " ");
-        }
-        System.out.println();
-
-        System.out.println("Total: " + getHandTotal(hand));
-    }
-
-    // Returns the highest possible score for a given hand.
-    private static int getHandTotal(ArrayList<String> hand) {
-        // TODO: Calculate the total value of a hand. If there is an Ace, use the higher value unless it causes a bust.
-        // Example: [A 8] should return 19. [A 6 9] should return 16.
-        int handTotal = 0;
-
-        for (int i = 0; i < hand.size(); i++){
-            String card = hand.get(i);
-            int cardValue = Integer.parseInt(card);
-            handTotal = handTotal + cardValue;
-        }
-        return handTotal;
-    }
-
-    // Returns true if a value is considered a bust. Returns false if the value is still playable.
-    private static boolean isValueBust(int handTotal) {
-
-        return handTotal > 21;
-
-
-    }
-}
--- BlackJack/src/Main.java
+++ BlackJack/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     // App entry point.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Blackjack game = new Blackjack();
         game.PlayGame();
     }
